bb204ac8b03139e88937ff2109d133e28059be9f
Imran Saleh <126408093+imransaleh21@users.noreply.github.com>
2025-07-01 02:06:24 +0600

    Program.cs file bug fixed for AddScoped service

--- StockPriceWebApp/Program.cs
+++ StockPriceWebApp/Program.cs
@@ -1,8 +1,9 @@
 using StockPrice.ServiceContracts;
+using StockPrice.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddScoped<IFinnhubService, IFinnhubService>();
+builder.Services.AddScoped<IFinnhubService, FinnhubService>();
 var app = builder.Build();
 
 app.UseStaticFiles();
